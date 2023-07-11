862f4cb7f311984c64d4317786e6e6928dc3a96c
Sk Md Sohel <mdsohel181@gmail.com>
2023-07-11 15:02:02 +0530

    updated logo

--- src/components/data/logo.js
+++ src/components/data/logo.js
@@ -1,2 +1,2 @@
 export const logo =
-  "https://raw.githubusercontent.com/skmdsohel/MyPortfolio/main/src/assets/logo/logoSohel.png?token=GHSAT0AAAAAACENOPUVW57CU6Y3Y7HUF7ZMZFNC65A";
+  "https://raw.githubusercontent.com/skmdsohel/MyPortfolio/main/src/assets/logo/logoSohel.png?token=GHSAT0AAAAAACENOPUVLB2F7LM34ZHMVWKEZFNEBKQ";
