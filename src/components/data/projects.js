0991a3e4bbbf024a7120bd64ee0eeb84c1f0f861
Sk Md Sohel <mdsohel181@gmail.com>
2023-07-11 15:48:27 +0530

    Added Project Screenshot

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -4,10 +4,21 @@ export const ProjectData = [
     title: "FoodStore Web Application",
     about:
       " FoodStore application is a convenient and user-friendly platform that allows customers to order food from various restaurants and have it delivered to their desired location. With just a few taps on their smartphones, users can browse through a wide range of cuisines, select their favorite dishes.",
-    tags: ["ReactJS", "Application Web Design", "Payment", "Web Development"],
+    tags: ["ReactJS", "React-Router-Dom", "Redux-toolkit", "Tailwind CSS"],
     demo: "https://github.com/skmdsohel/foodStore",
     github: "https://github.com/skmdsohel/foodStore",
     image:
       "https://raw.githubusercontent.com/skmdsohel/foodStore/main/src/assets/FoodStore.png",
-  }
+  },
+  {
+    id: 1,
+    title: "Portfolio Web App",
+    about:
+      " This is my portfolio website! Here, I showcase my creative journey and professional accomplishments. Through an elegant and minimalist design, website provides insights into my expertise and skills.",
+    tags: ["ReactJS", "Application Web Design", "Payment", "Web Development"],
+    demo: "https://github.com/skmdsohel/MyPortfolio/tree/main",
+    github: "https://github.com/skmdsohel/MyPortfolio/tree/main",
+    image:
+      "https://raw.githubusercontent.com/skmdsohel/MyPortfolio/main/Portfolio.png",
+  },
 ];
